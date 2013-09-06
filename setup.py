c89e3cea350fccff2416c839a9670ce0e563a5b4
Carl Sverre <carl@carlsverre.com>
2013-09-06 16:57:41 -0700

    even better testing

--- setup.py
+++ setup.py
@@ -10,11 +10,14 @@ class PyTest(TestCommand):
     user_options = [
         ('watch', 'w',
          "watch tests for changes"),
+        ('scan-directory=', 'd',
+         "only search for tests in the specified directory"),
     ]
     boolean_options = ['watch']
 
     def initialize_options(self):
         self.watch = False
+        self.scan_directory = None
         self.test_suite = None
         self.test_module = None
         self.test_loader = None
@@ -26,9 +29,17 @@ class PyTest(TestCommand):
         self.test_args = []
         if self.watch:
             self.test_args.append('-f')
+        if self.scan_directory is not None:
+            self.test_args.append(self.scan_directory)
 
     def run_tests(self):
-        import pytest, sys
+        import os, sys
+
+        MY_PATH = os.path.dirname(__file__)
+        sys.path.append(MY_PATH)
+        os.environ['PYTHONPATH'] = os.environ.get('PYTHONPATH', '') + ':' + MY_PATH
+
+        import pytest
         errno = pytest.main(self.test_args)
         raise sys.exit(errno)
 
