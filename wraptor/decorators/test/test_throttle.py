4867e249d3b12be25604c5e23281c17ba5116edb
zeke <zekevictor@gmail.com>
2018-05-04 17:58:19 -0700

    test forwards return val

--- wraptor/decorators/test/test_throttle.py
+++ wraptor/decorators/test/test_throttle.py
@@ -16,6 +16,14 @@ def test_basic():
 
     assert arr == [1, 1]
 
+def test_forwards_return_val():
+    @throttle(.1)
+    def test():
+        return 'foo'
+
+    assert test() is 'foo'
+    assert test() is None, 'return None when throttled'
+
 def test_fail_instance_method():
     """ Test that throttle without instance_method creates a globally
         shared throttle instance (shared by all instances of the class)
